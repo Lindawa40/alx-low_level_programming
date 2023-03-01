80f7bc6af10fcfd082263fa60fe6940fdd72b5f2
Linda <lindawachira40@gmail.com>
2023-02-28 17:21:54 -0800

    a function that reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 /*
- * void rev_string - print string in reverse
+ * rev_string - Reverses a string
  * @s: string to be reversed
  *
- * Return: nothing
+ * Return: reverse string
  */
+
 void rev_string(char *s)
 {
-	char s[10];
-	putchar(char *s);
-       strrev(*s);
-putchar(char*s);
+	int i, j, size;
+	char tmp;
+
+	if (!s)
+		return;
+	size = strlen(s);
+	j = size - 1;
+	for (i = 0; size / 2 > 1; i++)
+	{
+		tmp = s[i];
+		s[i] = s[j];
+		s[j] = tmp;
+		j--;
+	}
 }
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -3,4 +3,5 @@ I will be working with a series of pointers, arrys and strings in this folder
 1- a function that swaps the values of two integers
 2-a function that prints a string, followed by a new line, to stdout
 4-a function that prints a string, in reverse, followed by a new line
+5-a function that reverses a string
 
